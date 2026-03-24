4131486977726f2f51a0ef4c6ec075ce68aba5fc
bethixx <lisa39942@gmail.com>
2026-03-24 12:54:40 +0100

    Add project files.

---
+++ Data/LibraryContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Library.Models;
+
+namespace Library.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Category> Category { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Author)
+                .WithMany(a => a.Books)
+                .HasForeignKey(b => b.AuthorId);
+
+            modelBuilder.Entity<Book>()
+                .HasMany(b => b.Categories)
+                .WithMany(c => c.Books);
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public int AuthorId { get; set; }
+        public Author Author { get; set; } = null!;
+        public ICollection<Category> Categories { get; set; } = new List<Category>();
+    }
+}
