c21014a0546bbdfc9ec8981ae7058378e957f7b6
bethixx <lisa39942@gmail.com>
2026-04-14 16:27:17 +0200

    CRUD Books, Authors, filtering, validation

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,11 +1,15 @@
-﻿namespace Library.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
 {
     public class Book
     {
         public int Id { get; set; }
+        [MaxLength(200)]
+        [Required(ErrorMessage = "Tytuł jest wymagany")]
         public string Title { get; set; } = null!;
         public int AuthorId { get; set; }
-        public Author Author { get; set; } = null!;
+        public Author? Author { get; set; }
         public ICollection<Category> Categories { get; set; } = new List<Category>();
     }
 }
